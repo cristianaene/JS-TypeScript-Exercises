3ed269ac50f5a3dc39b186bc7117e2fb06101c17
Claudia Tomassetti <hagol.cker@gmail.com>
2021-07-07 14:52:38 +0200

    adds all basic exercises

--- Claudia/script.ts
+++ Claudia/script.ts
@@ -13,15 +13,41 @@
 
 //  //basic exercise 2
 
+//--- This version works! --
+const myName = []
+for (let i=0; i < 10; i++){
+    myName[i]= {fName: "Claudia", lName: "Tomassetti"}
+    
+}
+console.log(myName);
 
-// const myName = {"fName":"Claudia", "lName":"Tomassetti"};
+setTimeout(function(){
+    myName.forEach(function(i){
+        document.getElementById("full_name").innerHTML += i.fName + " " + i.lName + "<br>";
+    });
 
+}, 5000);
+
+
+//Ends --- This version works! --
+
+// //--- This version works! --
+// const myName = []
 // for (let i=0; i < 10; i++){
-//     myName.forEach(function(val){
-//         console.log(val);
-//     })
-// }
+    
+//     // const myName = {}
+//     // myName["fName"]= "Claudia";
+//     // myName["lName"]= "Tomassetti";
+//     myName[i]= {fName: "Claudia", lName :"Tomassetti"}
+//     // document.getElementById("full_name").innerHTML += myName[i].fName + myName[i].lName;
 
+//     console.log(myName);
+//     document.getElementById("full_name").innerHTML += myName[i].fName + " " + myName[i].lName + "<br>";
+// }
+// // Ends ---This version works!---
+
+
+// // ----This version doesn't work!!! ---
 // function creatingArray() {
 
 //     for (let i=0; i < 10; i++){
@@ -34,17 +60,43 @@
 //         console.log(myName);
 // }
 
-// }
-const myName = []
-for (let i=0; i < 10; i++){
-    
-    // const myName = {}
-    // myName["fName"]= "Claudia";
-    // myName["lName"]= "Tomassetti";
-    myName[i]= {fName: "Claudia", lName :"Tomassetti"}
-    // document.getElementById("full_name").innerHTML += myName[i].fName + myName[i].lName;
+// }  
+// End ----This version doesn't work!!! ---
 
-    console.log(myName);
-    document.getElementById("full_name").innerHTML += myName[i].fName;
-}
+// // ----This version doesn't work!!! ---
+// const myName = {"fName":"Claudia", "lName":"Tomassetti"};
+
+// for (let i=0; i < 10; i++){
+//     myName.forEach(function(val){
+//         console.log(val);
+//     })
+// }
+// // End ----This version doesn't work!!! ---
+
+
+ //Basic exercise 3
+
+// let allNames: string[] = ["Claudia", "Anna", "Laureen", "Alex", "Danilo"];
+
+//  //this prints the index
+// for (let i in allNames){
+//     document.getElementById("all_names").innerHTML += i + "<br>"
+// }
+
+// document.getElementById("all_names").innerHTML += "<br>"
+
+//  //this prints the value
+// for (let i of allNames){
+//     document.getElementById("all_names").innerHTML += i + "<br>"
+// }
+
+// document.getElementById("all_names").innerHTML += "<br>"
+
+//  //this prints the index and the value
+
+//  for (let i in allNames){
+//     document.getElementById("all_names").innerHTML += i + " " + allNames[i] + "<br>"
+// }
+
+
 
